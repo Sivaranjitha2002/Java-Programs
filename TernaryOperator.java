f8f7e27f6057a72c7aac09b23c3e786fe81a3d9c
Sivaranjitha C <77005105+Sivaranjitha2002@users.noreply.github.com>
2022-09-07 23:13:25 +0530

    Create TernaryOperator.java

---
+++ TernaryOperator.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class ternary{
+         public static void main(String a[]) throws Exception {
+                   Scanner s=new Scanner(System.in);
+                   System.out.println("------------------Check Vote Eligibility-------------------");
+                   System.out.println("Enter the age:");
+                   int age=Integer.parseInt(s.nextLine());
+                   System.out.println((age>18)?  "Eligible": "Not Eligible");
+                   
+         }
+}
+          
