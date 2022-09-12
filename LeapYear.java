f89b3c38193a9bbc172c8c3b9b078d572a33f02c
Sivaranjitha C <77005105+Sivaranjitha2002@users.noreply.github.com>
2022-09-12 16:31:06 +0530

    Create LeapYear.java

---
+++ LeapYear.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class prime{
+    public static void main(String a[]){
+        int i,j,k,l=0;
+        Scanner s=new Scanner(System.in);
+        System.out.print("Enter the Year:");
+        k=Integer.parseInt(s.nextLine());
+        if(k%4==0 && k%100!=0)
+             System.out.print("Leap Year");
+        else if(k%100==0 && k%400==0)
+             System.out.print("Leap year");
+        else 
+             System.out.print("Not a Leap Year");
+        
+    }
+}
