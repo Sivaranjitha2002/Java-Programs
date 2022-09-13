8553eb6883c7da9862f65590b07cd11bc280cb77
Sivaranjitha C <77005105+Sivaranjitha2002@users.noreply.github.com>
2022-09-13 21:18:52 +0530

    Create PrimeInterval.java

---
+++ PrimeInterval.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+public class Prime{
+    public static void main(String a[])throws Exception{
+        int i=0,j=0,k,l,h;
+        Scanner s=new Scanner(System.in);
+        System.out.print("Enter the Lower limit:");
+        l=s.nextInt();
+        System.out.print("Enter the higher limit:");
+        h=s.nextInt();
+        k=l;
+        while(k<=h){
+            l=k;j=0;
+            while(0<l){
+                i=l%10;
+                j+=(i*i*i);
+                l=l/10;
+            }
+            if(k==j)
+                System.out.println(j);
+            k++;
+        }
+    }
+}
