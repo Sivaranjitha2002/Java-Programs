bc661a286059a33a54d12e3ac35649fdf53ed350
Sivaranjitha C <77005105+Sivaranjitha2002@users.noreply.github.com>
2022-09-21 15:50:46 +0530

    Update ArrayLibrary.java

--- ArrayLibrary.java
+++ ArrayLibrary.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class SpaceRemover{
+public class ArrayLibrary{
     public static void main(String t[]) throws Exception{
         Scanner s=new Scanner(System.in);
         int[][] b={{1,2,3},{4,5,6},{7,8,9}};
