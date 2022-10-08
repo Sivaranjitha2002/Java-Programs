750761ed3425eaa651fa04abfc9174e5de9358b7
Sivaranjitha C <77005105+Sivaranjitha2002@users.noreply.github.com>
2022-10-08 18:35:31 +0530

    Create ConvertStringToInt.java

---
+++ ConvertStringToInt.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+
+    char a = '5';
+    char b = '9';
+
+    int num1 = Character.getNumericValue(a);
+    int num2 = Character.getNumericValue(b);
+
+    System.out.println(num1);   
+    System.out.println(num2);    
+  }
+}
