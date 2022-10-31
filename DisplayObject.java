46da75e9eaf5cd0b02ac4c4e5ab3d13e922e410f
Siva@2002 <sivaranjitha9843@gmail.com>
2022-10-31 20:04:07 +0530

    DisplayObject.java

---
+++ DisplayObject.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class sample{
+    public String toString(){
+           return "It is a Object";
+     }
+}
+class main{
+       public static void main(String n[]){
+                sample s=new sample();
+                System.out.println(s);
+       }
+} 
