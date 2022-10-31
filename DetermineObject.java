dbf45c4834c74c094b72302e920e2feffb09213f
Siva@2002 <sivaranjitha9843@gmail.com>
2022-10-31 20:01:15 +0530

    DetermineObject.java

---
+++ DetermineObject.java
@@ -0,0 +1,10 @@
+class c1{}
+class c2{}
+class main{
+           public static void main(String cc1[]){
+                      c1 cc=new c1();
+                      c2 ccc=new c2();
+                      System.out.println("cc is object of "+cc.getClass());
+                       System.out.println("ccc is object of "+ccc.getClass());
+            }
+}
