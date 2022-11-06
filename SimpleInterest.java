269bc495c2a26dfa2ef1b2dbfdba483cccf36f07
Siva@2002 <sivaranjitha9843@gmail.com>
2022-11-06 18:56:39 +0530

    SimpleInterest.java

---
+++ SimpleInterest.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class main{
+              public static void main(String dcx[]){
+                         Scanner s=new Scanner(System.in);
+                         System.out.print("Enter P: ");
+                          double p=s.nextDouble();
+                           System.out.print("Enter N: ");
+                          double n=s.nextDouble();
+                           System.out.print("Enter R: ");
+                          double r=s.nextDouble();
+                          double re=(p*n*r)/100;
+                          System.out.print("Simple Interest: "+re);
+             }
+}
