0cf60ceea5678b7510eba1fb9027a4c8a504a2bf
Siva@2002 <sivaranjitha9843@gmail.com>
2022-11-06 18:57:44 +0530

    StringToCharArray.java

---
+++ StringToCharArray.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Main{
+       public static void main(String asa[]){
+             Scanner s=new Scanner(System.in);
+              String ss=s.nextLine();
+              for(char c: ss.toCharArray())
+                              System.out.print(c+" ");
+       }
+}
