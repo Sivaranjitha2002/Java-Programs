ca2e5f884b3e7bd2b121402c9409f6b99029741b
Siva@2002 <sivaranjitha9843@gmail.com>
2022-11-06 18:57:14 +0530

    StringSwitchCase.java

---
+++ StringSwitchCase.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class main{
+              public static void main(String ff[]){
+                          Scanner s=new Scanner(System.in);
+                          String d=s.nextLine();
+                          switch(d){
+                                    case "one":System.out.println("I am first case");
+                                                          break;
+                                    case "two":System.out.println("I am second case");
+                                                          break;
+                                    case "three":System.out.println("I am third case");
+                                                          break;
+                                    case "four":System.out.println("I am fourth case");
+                                                          break;
+                                    case "five":System.out.println("I am fifth case");
+                                                          break;
+                         }
+            }
+}
