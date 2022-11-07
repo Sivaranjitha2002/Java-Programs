7519bff1ec66d77ab8fd066b03180813bc77f7cb
Siva@2002 <sivaranjitha9843@gmail.com>
2022-11-07 23:11:04 +0530

    GetOSName.java

---
+++ GetOSName.java
@@ -0,0 +1,7 @@
+class main{
+          public static void main(String kl[]){
+                      String os=System.getProperty("os.name");
+                      System.out.println("Operating System Name:"+os);
+                     System.out.println("Operating System Version:"+System.getProperty("os.version"));
+          }
+}
